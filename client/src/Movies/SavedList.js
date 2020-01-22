d41407a807a1e9267bae008d318097695ef6834a
unknown <tommyconner96@gmail.com>
2020-01-21 19:13:52 -0500

    mvp done

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
@@ -6,7 +8,9 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
+    <Link to= '/'>
     <div className="home-button">Home</div>
+    </Link>
   </div>
 );
 
